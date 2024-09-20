5227b82c3d5ffbedc14fa3dfde2985ae662aec1e
0xHarry2 <0xharry517@gmail.com>
2024-09-20 08:29:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
 from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import StreamingResponse
+from pathlib import Path
+import io
 
 app = FastAPI()
-app.mount("/result_uploads", StaticFiles(directory="result_uploads"), name="static")
-origins = [
-    "http://localhost:3000",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 from face_swap import face_swap
-
+IMAGES_DIR = Path("./result_uploads")
 
 @app.post("/face-swap")
 async def create_upload_file(avatar: UploadFile):
@@ -25,4 +24,14 @@ async def create_upload_file(avatar: UploadFile):
   with open(file_location, "wb+") as file_object:
       file_object.write(avatar.file.read())
   new_file_location = face_swap(file_location)
+  print(new_file_location)
   return {"result": new_file_location}
+
+@app.get("/result_uploads/{filename}")
+async def stream_image(filename: str):
+    file_path = IMAGES_DIR / filename
+    if file_path.exists() and file_path.is_file():
+        # Open the file and stream it
+        with open(file_path, "rb") as file:
+            return StreamingResponse(io.BytesIO(file.read()), media_type="image/jpeg")
+    return {"error": "File not found"}
